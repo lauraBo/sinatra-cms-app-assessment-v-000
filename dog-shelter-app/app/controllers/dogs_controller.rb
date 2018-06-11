8b863b6bcc0e661376ee9c361d2bdad96ef72811
Laura Bodell <bodelllaura@gmail.com>
2018-06-11 21:17:21 +0000

    added get route for creating a dog. created subdirectories for views for dogs and users

---
+++ dog-shelter-app/app/controllers/dogs_controller.rb
@@ -0,0 +1,9 @@
+class DogsController < Sinatra::Base
+  
+   get '/dogs/new' do
+   erb :'/dogs/new'
+   end 
+  
+  
+  
+end
---
+++ dog-shelter-app/app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < Sinatra::Base
+  
+  
+  
+end 
+

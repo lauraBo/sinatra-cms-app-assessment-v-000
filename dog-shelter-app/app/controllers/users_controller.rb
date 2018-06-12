592a4a043890d8885904d7f0015eb27f09bcb89a
Laura Bodell <bodelllaura@gmail.com>
2018-06-12 02:37:39 +0000

    added login get route and post sessions route to User Controller. Created login form in login erb file.

--- dog-shelter-app/app/controllers/users_controller.rb
+++ dog-shelter-app/app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController 
   
+  get '/sessions/login' do
+    erb :'sessions/login'
+  end
   
   
+  post '/sessions' do
+   user = User.find_by(:username => params[:username])
+
+ if user && user.authenticate(params[:password])
+  session[:user_id] = user.id
+  redirect '/users/home'
+  else
+   redirect “/“
+    end
+  end
 end 
 

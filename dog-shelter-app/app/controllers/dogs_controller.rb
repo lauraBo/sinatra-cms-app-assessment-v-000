87a47b7cc48f31105bc99e716b591b33e1ce22f0
Laura Bodell <bodelllaura@gmail.com>
2018-06-12 02:17:54 +0000

    created delete route in dogs controller

--- dog-shelter-app/app/controllers/dogs_controller.rb
+++ dog-shelter-app/app/controllers/dogs_controller.rb
@@ -6,8 +6,8 @@ class DogsController < ApplicationController
 end 
   
   
-  post '/dogs' do
-  @dog = Dog.create(:name => params[:name], :breed => params[:breed], :shelter => params[:shelter], :age => params[:age] )
+post '/dogs' do
+  @dog = Dog.create(:name => params[:name], :breed => params[:breed], :shelter => params[:shelter] :age => params[:age] )
   redirect to '/dogs/#{@dog.id}'
  end
 
@@ -18,23 +18,30 @@ end
 
 
 get '/dogs/:id' do
-@dog = Dog.find_by_id(params[:id])
-erb :'dogs/show'
+  @dog = Dog.find_by_id(params[:id])
+  erb :'dogs/show'
 end
 
 get '/dogs/:id/edit' do  #load edit form
- @dog = Dog.find_by_id(params[:id])
- erb :'dogs/edit'
+  @dog = Dog.find_by_id(params[:id])
+  erb :'dogs/edit'
 end
 
 patch '/dogs/:id' do #edit action
- @dog = Dog.find_by_id(params[:id])
- @dog.name = params[:name]
- @dog.breed = params[:breed]
- @dog.age = params[:age]
- @dog.shelter = params[:shelter]
- @dog.save
-redirect to '/dogs/#{@dog.id}'
+  @dog = Dog.find_by_id(params[:id])
+  @dog.name = params[:name]
+  @dog.breed = params[:breed]
+  @dog.age = params[:age]
+  @dog.shelter = params[:shelter]
+  @dog.save
+  redirect to '/dogs/#{@dog.id}'
 end
+
+delete '/dogs/:id/delete' do #delete action
+  @dog = Dog.find_by_id(params[:id])
+  @dog.delete
+  redirect to ‘/dogs'
+giend
+
 
 end

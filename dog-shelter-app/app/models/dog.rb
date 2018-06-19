12fc9e5817598bb110b95abf9b1234da9f62dfe0
Laura Bodell <bodelllaura@gmail.com>
2018-06-19 19:52:04 +0000

    updated Spec.md file. added validation method to Dog and User classes

--- dog-shelter-app/app/models/dog.rb
+++ dog-shelter-app/app/models/dog.rb
@@ -1,7 +1,7 @@
 class Dog < ActiveRecord::Base
   
   belongs_to :user 
-  
+  validates_presence_of :user_id, :name, :age, :breed, :shelter 
 end 
 
   
--- dog-shelter-app/app/models/user.rb
+++ dog-shelter-app/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
             
   has_many :dogs
   has_secure_password
-  
+   validates_presence_of :username, :email
 end 
